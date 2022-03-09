691f90a1faced1b6cccd0f7d3aec9271ea2ceec4
saitotm <tomokazu.ml.tech@gmail.com>
2022-03-09 15:40:45 +0900

    impl base of cui

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,32 @@ mod json_util;
 mod parser;
 mod tokenizer;
 
+use std::collections::VecDeque;
+
+use clap::Parser;
+use generator::Generator;
+use tokenizer::Token;
+
+use crate::tokenizer::Tokenizer;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Json  be pretty
+    #[clap(short, long)]
+    json: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let tokenizer = Tokenizer::new(args.json);
+    let tokens = tokenizer.collect::<Result<VecDeque<Token>, _>>().unwrap();
+
+    let mut parser = parser::Parser::new(tokens);
+    let node = parser.parse().unwrap();
+
+    let gen = Generator::new(node);
+    println!("{}", gen.generate());
 }
